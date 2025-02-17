8928ce545eb7cc7723a8108f5e20dc2c6939dc66
baisali609 <baisali609@gmail.com>
2025-02-17 22:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 let obj={};
-for(let i=0;i<band.length;i++){
-    let newArr = band[i].split(" ")
+for(let i=0;i<bands.length;i++){
+    let newArr = bands[i].split(" ")
     console.log(newArr);
     if(newArr[0]=="A"|| newArr[0]=="An"||newArr[0]=="The"){
         newArr.splice(0,1);
