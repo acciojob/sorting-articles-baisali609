e5da72e5745ca9cb654e514a983519e8e9603f1c
baisali609 <baisali609@gmail.com>
2025-02-17 22:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let obj={};
+for(let i=0;i<arr.length;i++){
+    let newArr = arr[i].split(" ")
+    console.log(newArr);
+    if(newArr[0]=="A"|| newArr[0]=="An"||newArr[0]=="The"){
+        newArr.splice(0,1);
+    }
+    console.log(newArr)
+    obj[arr[i]] = newArr.join(' ');
+    console.log(obj);
+}
